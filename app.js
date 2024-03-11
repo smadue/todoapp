04aa1dc486d3904af1bd4958b186ab95d194c5e9
smadue <waleakaje@gmail.com>
2024-03-10 23:53:26 -0700

    Todolist is now printing to screen

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// Top date and time
+function addTodo() {
+  let taskTitle = document.getElementById("taskTitle");
+  let taskDesc = document.getElementById("taskDesc");
+  let todos = document.getElementById("todos");
+
+  if (taskTitle.value.trim() === "" || taskDesc.value.trim() === "") {
+    alert("You must enter a todo title and description");
+  }
+
+  let li = document.createElement("li");
+  li.textContent = taskTitle.value;
+
+  let span = document.createElement("span");
+  span.textContent = taskDesc.value;
+
+  todos.appendChild(li);
+  todos.appendChild(span);
+}
