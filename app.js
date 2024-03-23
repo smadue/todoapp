22c37c957bbc64c2e8430165e6ec1a1bb9bcb076
smadue <waleakaje@gmail.com>
2024-03-23 01:51:02 -0700

    todo input append to screen from form

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ function addTodo() {
   let taskDesc = document.getElementById("taskDesc");
   let todos = document.getElementById("todos");
 
-  if (taskTitle.value.trim() === "" || taskDesc.value.trim() === "") {
+  if (taskTitle.value.trim() === "") {
     alert("You must enter a todo title and description");
   }
 
   let li = document.createElement("li");
-  li.textContent = taskTitle.value;
-
   let span = document.createElement("span");
+  let hr = document.createElement("hr");
+
+  // let radio = document.createElement("input");
+  // radio.setAttribute("type", "checkbox");
+
+  li.textContent = taskTitle.value;
   span.textContent = taskDesc.value;
 
   todos.appendChild(li);
-  todos.appendChild(span);
+  todos.appendChild(hr);
 }
